0a30c2947c409498a55624dfd69c64c5cc6fa13b
ibrahemnaser <i.naser4040@gmail.com>
2026-01-16 15:42:53 +0200

    Re-initialize repo

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        /*
-        * A CLI task manager
-        * - each task: {int ID, String Description, String Priority}
-        * - task manager: {display menu ['1- add new task',
-        *                                '2- view all tasks',
-        *                                '3- view a task',
-        *                                '4- delete a task'] }
-        * */
-
-        new TaskManager();
-
-        /*
-        * Learn File Handling
-        * */
-
-//        new MyFileHandler();
+        System.out.println("*****************");
+        System.out.println("JAVA CLI PROJECTS");
+        System.out.println("*****************");
     }
 }
